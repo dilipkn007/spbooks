ad205c260f98696370e569c9855b9fe5d1ee5085
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-02-23 02:39:55 +0000

    changes in back nav icon in the book reading screen

--- lib/reader_view/reader_view_widget.dart
+++ lib/reader_view/reader_view_widget.dart
@@ -5,6 +5,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'reader_view_model.dart';
@@ -192,14 +193,13 @@ class _ReaderViewWidgetState extends State<ReaderViewWidget> {
                   children: [
                     FlutterFlowIconButton(
                       buttonSize: 40.0,
-                      icon: Icon(
-                        Icons.close_rounded,
+                      icon: FaIcon(
+                        FontAwesomeIcons.arrowLeft,
                         color: FlutterFlowTheme.of(context).primaryText,
                         size: 24.0,
                       ),
                       onPressed: () async {
-                        _model.utilityFlag = !_model.utilityFlag;
-                        safeSetState(() {});
+                        context.safePop();
                       },
                     ),
                     Row(
