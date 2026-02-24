c281b2afb73baa9a159d958014154d8a436ca8ab
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-02-24 13:11:57 +0000

    Updating to latest FlutterFlow output.

--- lib/main_screens/books_library/books_library_widget.dart
+++ lib/main_screens/books_library/books_library_widget.dart
@@ -445,7 +445,7 @@ class _BooksLibraryWidgetState extends State<BooksLibraryWidget> {
                             padding: EdgeInsets.zero,
                             gridDelegate:
                                 SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 3,
+                              crossAxisCount: 2,
                               crossAxisSpacing: 20.0,
                               mainAxisSpacing: 24.0,
                               childAspectRatio: 0.65,
